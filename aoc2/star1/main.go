867085da76f59c78ab964bf91f7a23802e6f5842
Lyvric <lyvric@yvtils.net>
2025-12-12 15:25:57 +0100

    Add input data and implement initial logic for range processing in star 1

--- aoc2/star1/main.go
+++ aoc2/star1/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
+var (
+	invalidIDs []string
+)
 
 func main() {
 	data, err := os.ReadFile("../input.txt")
@@ -12,8 +17,34 @@ func main() {
 		panic(err)
 	}
 
-	lines := strings.SplitSeq(string(data), "\n")
+	lines := strings.SplitSeq(string(data), ",")
 
 	for line := range lines {
+		rangeOne, err := strconv.Atoi(strings.Split(line, "-")[0])
+		if err != nil {
+			panic(err)
+		}
+
+		rangeTwo, err := strconv.Atoi(strings.Split(line, "-")[1])
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(rangeOne, rangeTwo)
+
+		for i := rangeOne; i <= rangeTwo; i++ {
+			fmt.Println("ID:", i)
+
+			splitedID := strings.Split(strconv.Itoa(i), "")
+			cSplitedID := splitedID
+
+			for id := range splitedID {
+				for id2 := range cSplitedID {
+					if id != id2 {
+						break
+					}
+				}
+			}
+		}
 	}
 }
